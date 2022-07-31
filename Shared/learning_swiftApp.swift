efc9b93749dd30d46640a7bcd19f5bbed05f8784
Abidemi Ope <opeabidemi@gmail.com>
2022-07-31 20:29:21 +0100

    Initial Commit

---
+++ Shared/learning_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_swiftApp.swift
+//  Shared
+//
+//  Created by Harry on 31/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct learning_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
